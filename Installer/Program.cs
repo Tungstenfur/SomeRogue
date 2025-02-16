88c84c8bfd7da8e60aabc573ce221809180f7aeb
Koszutowski <89244968+Tungstenfur@users.noreply.github.com>
2025-02-16 11:57:17 +0100

    Installer part. 1

---
+++ Installer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using Spectre.Console;
+Main();
+void Main()
+{
+    Console.Title = "SomeRogue Installer";
+    var font = FigletFont.Load("Doom.flf");
+    var rule = new Rule("[White]Installer[/]");
+    AnsiConsole.Write(new FigletText(font, "SomeRogue").Centered().Color(Color.White));
+    AnsiConsole.Write(rule);
+    var op = AnsiConsole.Prompt( new SelectionPrompt<string>()
+        .AddChoices(new[] { "Install", "Uninstall","Update", "Exit" }));
+    if(op=="Install")
+        Install();
+}
+
+void Install()
+{
+    AnsiConsole.Clear();
+    AnsiConsole.Status().Start("Installing", ctx =>
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            AnsiConsole.MarkupLine("[blue]Operating system is Windows[/]");
+            if(new Ping().Send("github.com").Status==IPStatus.Success)
+            {
+                AnsiConsole.MarkupLine("[blue]Internet connection is available[/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[bold red]Internet connection is not available[/]");
+                Console.ReadKey();
+            }
+        }
+    });
+}
